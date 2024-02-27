73244abfde7c26d68c5338cdd2f87a336736c579
Frederik Winther Bæk <frederik.baek@hotmail.com>
2024-02-27 17:34:55 +0100

    yes

--- test.py
+++ test.py
@@ -7,3 +7,9 @@ f=open("authors.txt","r",encoding="utf-8")
 url="https://api.openalex.org/works?per-page=200&filter=author.id:https://openalex.org/A5035210768"
 r=requests.get(url).json()
 print(r)
+
+ 
+my_list = ['geeks', 'for', 'geeks']
+another_list = [6, 0, 4, 1]
+my_list.append(another_list)
+print (type(my_list[3]))
