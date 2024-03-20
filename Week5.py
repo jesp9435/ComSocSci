8bf6de46620cb887c2d62dcf7c2e3efd8699e3a3
Frederik Winther Bæk <frederik.baek@hotmail.com>
2024-03-20 16:30:59 +0100

    Changed stuff

--- Week5.py
+++ Week5.py
@@ -194,3 +194,25 @@ print(cumulative_std_error)
 # All other network characteristics, from the degree distribution to the clustering coefficient, are significantly different in real networks. 
 
 # Part 3:
+import csv
+import networkx as nx
+import netwulf as nw
+import numpy as np
+edges = []
+amount_of_edges = 0
+with open("weighted_edgelist.txt","r") as f:
+    edges = list(tuple(line) for line in csv.reader(f))
+for edge in edges:
+    amount_of_edges += int(edge[2])
+
+amount_of_nodes = 79264
+print(amount_of_edges)
+
+probability = amount_of_edges/((amount_of_edges*(amount_of_edges-1))/2)
+print(probability)
+
+k = probability*(amount_of_edges-1.0)
+print(k)
+
+RG = nx.gnp_random_graph(amount_of_edges, probability, seed=1000, directed=False)
+nx.draw(RG)
--- test.py
+++ test.py
@@ -1,15 +1,21 @@
-import requests
-import pandas as pd
-import concurrent.futures 
-import requests
-import math
-f=open("authors.txt","r",encoding="utf-8")
-url="https://api.openalex.org/works?per-page=200&filter=author.id:https://openalex.org/A5035210768"
-r=requests.get(url).json()
-print(r)
+import csv
+import networkx as nx
+import netwulf as nw
+edges = []
+amount_of_edges = 0
+with open("weighted_edgelist.txt","r") as f:
+    edges = list(tuple(line) for line in csv.reader(f))
+for edge in edges:
+    amount_of_edges += int(edge[2])
 
- 
-my_list = ['geeks', 'for', 'geeks']
-another_list = [6, 0, 4, 1]
-my_list.append(another_list)
-print (type(my_list[3]))
+amount_of_nodes = 79264
+print(amount_of_edges)
+
+probability = amount_of_edges/((amount_of_edges*(amount_of_edges-1))/2)
+print(probability)
+
+k = probability*(amount_of_edges-1.0)
+print(k)
+
+RG = nx.gnp_random_graph(amount_of_edges, probability, seed=1000, directed=False)
+nw.visualize(RG)
